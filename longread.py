9c96994d466328c25e8c0f4d5ddbe05d26da65d3
Patrick Berry <pberry@gmail.com>
2018-11-23 15:40:56 -0800

    Update longread.py

--- longread.py
+++ longread.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import feedparser
 import re
+import time
 from bs4 import BeautifulSoup
 
 # d = feedparser.parse('https://daringfireball.net/thetalkshow/rss')
@@ -8,10 +9,10 @@ d = feedparser.parse('https://techmeme.com/techmeme-ride-home-feed')
 for post in d.entries:
    	if "Longread" in post.summary :
    		soup = BeautifulSoup(post.summary, 'html.parser')
+   		postPub = time.strftime("%A, %B %d" ,post.published_parsed)
+   		print(postPub)
    		for p in soup.find_all('p'):
    			if "Longreads:" in p.text:
-   				print ("\n**" + post.published + "**")
+   				print ("\n**" + postPub + "**")
    				for a in (p.next_sibling.find_all('a')):
    					print ("* [" + a.string + "](" + a['href'] + ")")
-
-	
